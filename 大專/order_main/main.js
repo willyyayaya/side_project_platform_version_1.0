058502df395b21d3c993430285c8208f3e0465aa
eru-86 <eruliu1011@gmail.com>
2025-01-08 16:27:04 +0800

    1/8 完成收藏按鈕(大概)

--- 大專/order_main/main.js
+++ 大專/order_main/main.js
@@ -23,25 +23,70 @@ $(document).ready(async function () {
     let responseSkill = await fetch(skillsUrl);
     let responseSkillToJSON = await responseSkill.json();
     console.log(responseSkillToJSON);
-    skill.innerText = '需要技能 : ' + responseSkillToJSON ;
-
+    skill.innerText = '需要技能 : ' + responseSkillToJSON;
 
 
     //會員資料
     //顯示發案者頭像和名字
-    let ordermemberUrl = `http://localhost:8080/api/memberOrders/getMembersByOrderId/113`;
-    let responseOrdermember = await fetch(ordermemberUrl);
-    let responseOrdermemberToJSON = await responseOrdermember.json();
-    console.log(responseOrdermemberToJSON);
-    imgBorder.innerHTML = `<img class="img-fluid object-fit-contain" src="${responseOrdermemberToJSON[0].picurl}">`;
-    memberName.innerText = `${responseOrdermemberToJSON[0].name}`;
+    //1.抓取會員資料
+    let memberUrl = `http://localhost:8080/api/memberOrders/getMemberIdByOrderId/${orderId}`;
+    let responseMember = await fetch(memberUrl);
+    let responseMemberToJSON = await responseMember.json();
+    console.log(responseMemberToJSON);
+    console.log(responseMemberToJSON[0].memberid);
+    //2.以會員id去拿個人資料
+    imgBorder.innerHTML = `<img class="img-fluid object-fit-contain" src="${responseMemberToJSON[0].picurl}">`;
+    memberName.innerText = `${responseMemberToJSON[0].name}`;
+
+    let rankUrl = `http://localhost:8080/api/memberOrders/getRank/${responseMemberToJSON[0].memberid}`;
+    let responseRank = await fetch(rankUrl);
+    let responseRankToJSON = await responseRank.json();
+    console.log(responseRankToJSON);
+    rank.innerText = '評價:' + (responseRankToJSON).toFixed(2) + '分';
+
 
     //內容
     detail.innerHTML = responseOrderToJSON.detail;
 
+    //收藏按鈕
+    collect.onclick = function () {
+        let ownedUrl = 'http://localhost:8080/api/memberOrders/addWantedOrder';
+        fetch(ownedUrl, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                memberId: responseMemberToJSON[0].memberid,
+                orderId: orderId,
+                owned: 0,
+                wanted: 1,
+                collected: 0
+            })
+        })
+
+        // .then(response => {
+        //     if (response.ok) {
+        //         // 如果請求成功，更新按鈕文字
+        //         $('#collect').text = '已收藏';
+        //     } else {
+        //         // 如果有錯誤，顯示錯誤提示
+        //         alert("錯誤: 無法標記為感興趣");
+        //     }
+        // })
+        // .catch(error => {
+        //     console.error("發生錯誤:", error);
+        //     alert("發生錯誤，請稍後再試");
+        // });
+    };
 
 
 
+    //推薦其他專案
+    let anotherUrl = 'http://localhost:8080/api/orders/getAllOrders';
+    let responseAnother = await fetch(anotherUrl);
+    let responseAnotherToJSON = await responseAnother.json();
+    console.log(responseAnotherToJSON);
 
 
 });
