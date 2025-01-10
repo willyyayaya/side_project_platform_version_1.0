aa456d22791a47e8af73b8dc12a6f2a544e7eeb2
eru-86 <eruliu1011@gmail.com>
2025-01-10 15:40:56 +0800

    1/10 新增一些提醒

--- 大專/order_edit/edit.js
+++ 大專/order_edit/edit.js
@@ -14,7 +14,9 @@ $(document).ready(async function () {
                 [{ 'size': ['small', false, 'large', 'huge'] }], // 文字大小
                 [{ 'header': [1, 2, 3, 4, 5, 6, false] }],// 標題
                 [{ 'color': [] }, { 'background': [] }], // 顏色
-                [{ 'font': [] }], // 字體
+                ['image'], // add's image support
+                // ['link'],
+                // [{ 'font': [] }], // 字體
                 [{ 'align': [] }], // 文字方向
                 ['clean'] // 清除文字格是
             ]
@@ -22,12 +24,29 @@ $(document).ready(async function () {
         placeholder: '請在此輸入內容...'
     });
 
-
-
     //地點選單
-    $("#region").append('<option>1</option>');
-    $("#region").append('<option>2</option>');
-    $("#region").append('<option>3</option>');
+    $("#region").append('<option>台北市</option>');
+    $("#region").append('<option>新北市</option>');
+    $("#region").append('<option>桃園市</option>');
+    $("#region").append('<option>台中市</option>');
+    $("#region").append('<option>台南市</option>');
+    $("#region").append('<option>高雄市</option>');
+    $("#region").append('<option>基隆市</option>');
+    $("#region").append('<option>新竹市</option>');
+    $("#region").append('<option>嘉義市</option>');
+    $("#region").append('<option>宜蘭縣</option>');
+    $("#region").append('<option>新竹縣</option>');
+    $("#region").append('<option>苗栗縣</option>');
+    $("#region").append('<option>彰化縣</option>');
+    $("#region").append('<option>南投縣</option>');
+    $("#region").append('<option>雲林縣</option>');
+    $("#region").append('<option>嘉義縣</option>');
+    $("#region").append('<option>屏東縣</option>');
+    $("#region").append('<option>花蓮縣</option>');
+    $("#region").append('<option>台東縣</option>');
+    $("#region").append('<option>澎湖縣</option>');
+    $("#region").append('<option>金門縣</option>');
+    $("#region").append('<option>連江縣</option>');
     //需求技能(抓資料庫內容)
     // $("#skill").append('<input type="checkbox" name="needSkill" value="Python">');
     // $("input[value='Python']").after('<span>' + $('input[value="Python"]').val() + '</span>');
@@ -52,6 +71,20 @@ $(document).ready(async function () {
         $("#skill").append(checkbox).append(span);
     });
 
+    $("input[id='casutamu']").css("display", "none");
+    if ($('input[value="自訂"]').prop('checked')) {
+        $('input[id="casutamu"]').css("display", "inline-block");
+    } else if ($('input[value="另議"]').prop('checked')) {
+        $("input[id='casutamu']").css("display", "none");
+    }
+
+    $('input[name="budget"]').change(function () {
+        if ($('input[value="自訂"]').prop('checked')) {
+            $('input[id="casutamu"]').css("display", "inline-block");
+        } else if ($('input[value="另議"]').prop('checked')) {
+            $("input[id='casutamu']").css("display", "none");
+        }
+    })
 
     $("select[name='region']").css("display", "none");
     if ($('input[value="region"]').prop('checked')) {
@@ -68,13 +101,97 @@ $(document).ready(async function () {
         }
     })
 
-
     insert.onclick = function () {
+        // 檢查必填欄位並提示
+        let empty = '請填寫';
+
+        // 檢查專案標題是否填寫
+        if ($("#projectTitle").val() === "") {
+            empty += ' 專案標題';
+        }
+        // 檢查簡單介紹是否填寫
+        if ($('#simpleInfo').val() === "") {
+            empty += ' 簡介';
+        }
+        // 檢查類別是否選擇
+        if ($("#category").val() === "") {
+            empty += ' 類型';
+        }
+        // 檢查預算是否選擇
+        if ($('input[name="budget"]:checked').length === 0) {
+            empty += ' 預算';
+        } else {
+            // 如果預算選擇了 "自訂"，則檢查金額是否填寫
+            if ($('input[name="budget"]:checked').val() === "自訂" && $('#casutamu').val() === "") {
+                empty += ' 預算金額';
+            }
+        }
+        // 檢查工作地點是否選擇
+        if ($('input[name="workplace"]:checked').length === 0) {
+            empty += ' 工作地點';
+        } else {
+            // 如果選擇了 "region"，檢查地區是否填寫
+            if ($('input[name="workplace"]:checked').val() === "region" && $('#region').val() === "") {
+                empty += ' 工作地區';
+            }
+        }
+        // 檢查是否選擇了技能
+        if ($("input[name='needSkill']:checked").length === 0) {
+            empty += ' 至少一項技能';
+        }
+        // 檢查人數和截止日期是否填寫
+        if ($('#people').val() === "") {
+            empty += ' 人數';
+        }
+        if ($('#deadline').val() === "") {
+            empty += ' 截止日期';
+        }
+        // // 檢查圖片是否有上傳
+        // if ($('img').prop("src") === "") {
+        //     empty += '，請上傳圖片';
+        // }
+        // 如果有任何欄位未填寫，則不繼續執行表單提交
+        if (empty != '請填寫') {
+            alert(empty);
+            if (empty.includes(' 專案標題')) {
+                $('#projectTitle').focus();
+            } else if (empty.includes(' 簡介')) {
+                $('#simpleInfo').focus();
+            } else if (empty.includes(' 類型')) {
+                $('#category').focus();
+            } else if (empty.includes(' 預算')) {
+                $('input[name="budget"]:checked').focus();
+            } else if (empty.includes(' 預算金額')) {
+                $('#casutamu').focus();
+            } else if (empty.includes(' 工作地點')) {
+                $('input[name="workplace"]:checked').focus();
+            } else if (empty.includes(' 工作地區')) {
+                $('#region').focus();
+            } else if (empty.includes(' 至少一項技能')) {
+                $("input[name='needSkill']:checked").focus();
+            } else if (empty.includes(' 人數')) {
+                $('#people').focus();
+            } else if (empty.includes(' 截止日期')) {
+                $('#deadline').focus();
+            } 
+            // else if (empty.includes(' 圖片')) {
+            //     $('img').focus();
+            // }
+            return; // 阻止提交表單
+        }
+
         console.log($('#simpleInfo').val());
         console.log($("#category").val());
         console.log($("#projectTitle").val());
         console.log($("input[name='budget']:checked").val());
+        if ($('input[name="budget"]:checked').val() == "自訂") {
+            console.log($('#casutamu').val());
+            var money = $('#casutamu').val();
+        } else if ($('input[name="budget"]:checked').val() == "另議") {
+            console.log($('input[value="另議"]').val());
+            var money = $('input[value="另議"]').val();
 
+        }
         if ($('input[name="workplace"]:checked').val() == "region") {
             console.log($('#region').val());
             var rigion = $('#region').val();
@@ -83,24 +200,16 @@ $(document).ready(async function () {
             var rigion = $('input[value="遠端工作"]').val();
 
         }
-
         var selectedSkills = [];
         $("input[name='needSkill']:checked").each(function () {
             selectedSkills.push($(this).val());
         });
         console.log(selectedSkills);
-
         console.log($('#people').val());
         console.log($('#deadline').val());
-
         console.log($('img').prop("src"));
-
         var htmlContent = quill.root.innerHTML; // 獲取 HTML 內容
         console.log(htmlContent);
-
-
-
-
 
         //上傳專案表單
         let orderUrl = 'http://localhost:8080/api/orders/addOrder';
@@ -118,11 +227,10 @@ $(document).ready(async function () {
                 location: rigion,
                 people: $('#people').val(),
                 tagIds: selectedSkills,
-                budget: $("input[name='budget']:checked").val(),
+                budget: money,
                 upload: new Date().toISOString().split('T')[0],
                 newdate: new Date().toISOString().split('T')[0],
                 category: $("#category").val(),
-
             })
         }).then(response => response.json()
         ).then(data => {
@@ -151,21 +259,16 @@ $(document).ready(async function () {
                 console.error('未獲取到 orderId');
                 alert("無法獲取訂單ID，請再試一次。");
             }
-
         }).catch(error => {
             console.error("發生錯誤：", error);
             alert("發生錯誤，請稍後再試。"); // 顯示錯誤提示
         });
-
-
     }
-
 
     // 顯示縮圖用
     imageInput.addEventListener('change', function () {
         const file = imageInput.files[0];
         console.log(imageInput.files[0]);
-
         if (file) {
             const reader = new FileReader();
             reader.onload = function (e) {
@@ -175,8 +278,6 @@ $(document).ready(async function () {
             reader.readAsDataURL(file);
         }
     });
-
-
 });
 
 
--- 大專/order_main/main.js
+++ 大專/order_main/main.js
@@ -16,70 +16,17 @@ $(document).ready(async function () {
     updateDate.innerText = '最新更新時間:' + responseOrderToJSON.newdate;
     budget.innerText = '預算 : ' + responseOrderToJSON.budget;
     area.innerText = '地點 : ' + responseOrderToJSON.location;
-    head.innerText = '建議人數 : ' + responseOrderToJSON.people;
+    head.innerText = '建議人數 : ' + responseOrderToJSON.people + '人';
     deadline.innerText = '申請截止日 : ' + responseOrderToJSON.deadline;
     //需要技能
-    let skillsUrl = `http://localhost:8080/api/tags/getTagNames`;
+    let skillsUrl = `http://localhost:8080/api/tags/getTag/${orderId}`;
     let responseSkill = await fetch(skillsUrl);
     let responseSkillToJSON = await responseSkill.json();
     console.log(responseSkillToJSON);
     skill.innerText = '需要技能 : ' + responseSkillToJSON;
 
-
-    //會員資料
-    //顯示發案者頭像和名字
-    //1.抓取會員資料
-    let memberUrl = `http://localhost:8080/api/memberOrders/getMemberIdByOrderId/${orderId}`;
-    let responseMember = await fetch(memberUrl);
-    let responseMemberToJSON = await responseMember.json();
-    console.log(responseMemberToJSON);
-    console.log(responseMemberToJSON[0].memberid);
-    //2.以會員id去拿個人資料
-    imgBorder.innerHTML = `<img class="img-fluid object-fit-contain" src="${responseMemberToJSON[0].picurl}">`;
-    memberName.innerText = `${responseMemberToJSON[0].name}`;
-
-    let rankUrl = `http://localhost:8080/api/memberOrders/getRank/${responseMemberToJSON[0].memberid}`;
-    let responseRank = await fetch(rankUrl);
-    let responseRankToJSON = await responseRank.json();
-    console.log(responseRankToJSON);
-    rank.innerText = '評價:' + (responseRankToJSON).toFixed(2) + '分';
-
-
     //內容
     detail.innerHTML = responseOrderToJSON.detail;
-
-    //收藏按鈕
-    collect.onclick = function () {
-        let ownedUrl = 'http://localhost:8080/api/memberOrders/addWantedOrder';
-        fetch(ownedUrl, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({
-                memberId: responseMemberToJSON[0].memberid,
-                orderId: orderId,
-                owned: 0,
-                wanted: 1
-            })
-        })
-
-        // .then(response => {
-        //     if (response.ok) {
-        //         // 如果請求成功，更新按鈕文字
-        //         $('#collect').text = '已收藏';
-        //     } else {
-        //         // 如果有錯誤，顯示錯誤提示
-        //         alert("錯誤: 無法標記為感興趣");
-        //     }
-        // })
-        // .catch(error => {
-        //     console.error("發生錯誤:", error);
-        //     alert("發生錯誤，請稍後再試");
-        // });
-    };
-
-
 
     //推薦其他專案
     let anotherUrl = 'http://localhost:8080/api/orders/getAllOrders';
@@ -112,6 +59,36 @@ $(document).ready(async function () {
         $('#another').append(anotherpro);
     }
 
+    //收藏按鈕
+    collect.onclick = function () {
+        let ownedUrl = 'http://localhost:8080/api/memberOrders/addWantedOrder';
+        fetch(ownedUrl, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                memberId: responseMemberToJSON[0].memberid,
+                orderId: orderId,
+                owned: 0,
+                wanted: 1
+            })
+        })
+
+        // .then(response => {
+        //     if (response.ok) {
+        //         // 如果請求成功，更新按鈕文字
+        //         $('#collect').text = '已收藏';
+        //     } else {
+        //         // 如果有錯誤，顯示錯誤提示
+        //         alert("錯誤: 無法標記為感興趣");
+        //     }
+        // })
+        // .catch(error => {
+        //     console.error("發生錯誤:", error);
+        //     alert("發生錯誤，請稍後再試");
+        // });
+    };
 
     //顯示申請人數
     let applypeopleUrl = `http://localhost:8080/api/memberOrders/collected/people/${orderId}`;
@@ -120,8 +97,6 @@ $(document).ready(async function () {
     console.log(responsePeopleToJSON);
     let ap = '目前申請人數:' + responsePeopleToJSON + '人';
     $('#applyPeople').text(ap);
-
-
 
     //申請按鈕
     apply.onclick = function () {
@@ -138,4 +113,21 @@ $(document).ready(async function () {
             })
         })
     }
+    //會員資料
+    //顯示發案者頭像和名字
+    //1.抓取會員資料
+    let memberUrl = `http://localhost:8080/api/memberOrders/getMemberIdByOrderId/${orderId}`;
+    let responseMember = await fetch(memberUrl);
+    let responseMemberToJSON = await responseMember.json();
+    console.log(responseMemberToJSON);
+    console.log(responseMemberToJSON[0].memberid);
+    //2.以會員id去拿個人資料
+    imgBorder.innerHTML = `<img class="img-fluid object-fit-contain" src="${responseMemberToJSON[0].picurl}">`;
+    memberName.innerText = `${responseMemberToJSON[0].name}`;
+
+    let rankUrl = `http://localhost:8080/api/memberOrders/getRank/${responseMemberToJSON[0].memberid}`;
+    let responseRank = await fetch(rankUrl);
+    let responseRankToJSON = await responseRank.json();
+    console.log(responseRankToJSON);
+    rank.innerText = '評價:' + (responseRankToJSON).toFixed(2) + '分';
 });
